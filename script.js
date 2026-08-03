08610175ad4364c43f4d6bd70e0190d2632193c7
bibxo <127908839+bibxo@users.noreply.github.com>
2026-08-03 20:57:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const img = document.getElementById('whim-img');
+const input = document.getElementById('user-input');
+
+const defaultImg = 'images/normal.png';
+const happyImg = 'images/happy.png';
+const triggeredImg = 'images/trigger.png';
+
+const triggerWords = ['work', 'job', 'gay', 'jesus', 'musab', 'boobjoob', 'accountability', 'faggot', 'nigger', 'willow', 'sexism', 'father', 'ai', 'ex', 'boyfriend', 'ableton', 'fl studio', 'recording', 'mp4 to mp3', 'downloading', 'doomscroll', 'bullshit', 'queer', 'rich', 'boys'];
+const happyWords = ['dnd', 'racism', 'racist', 'language', 'culture', 'blender', 'art', 'poor', 'broke', 'music', 'starter loops', 'production', 'girls', 'creativity', 'unemployment', 'laziness', 'adhd', 'ocd', 'autism']
+const arabicRegex = /[\u0600-\u06FF]+/;
+
+input.addEventListener('input', (e) => {
+    const text = e.target.value.toLowerCase();
+    const words = text.split(/[^a-z0-9\u0600-\u06ff]+/).filter(Boolean);
+    const containsArabic = arabicRegex.test(text);
+
+    let state = 'default';
+
+    for (let i = words.length - 1; i >= 0; i--) {
+        const word = words[i];
+
+        if (happyWords.includes(word)) {
+            state = 'happy';
+            break;
+        }
+
+        if (triggerWords.includes(word) || containsArabic) {
+            state = 'triggered';
+            break;
+        }
+    }
+
+    img.classList.remove('triggered', 'happy');
+
+    if (state === 'happy') {
+        img.src = happyImg;
+        img.classList.add('happy');
+    } else if (state === 'triggered') {
+        img.src = triggeredImg;
+        img.classList.add('triggered');
+    } else {
+        img.src = defaultImg;
+    }
+});
